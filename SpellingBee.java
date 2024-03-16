388b5c8a8a29c41c97d2817ad17154d688b2d783
victoriahbradley <112740389+victoriahbradley@users.noreply.github.com>
2024-03-15 23:06:59 -0700

    Add files via upload

---
+++ SpellingBee.java
@@ -0,0 +1,202 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Spelling Bee
+ *
+ * This program accepts an input of letters. It prints to an output file
+ * all English words that can be generated from those letters.
+ *
+ * For example: if the user inputs the letters "doggo" the program will generate:
+ * do
+ * dog
+ * doggo
+ * go
+ * god
+ * gog
+ * gogo
+ * goo
+ * good
+ *
+ * It utilizes recursion to generate the strings, mergesort to sort them, and
+ * binary search to find them in a dictionary.
+ *
+ * @author Zach Blick, [ADD YOUR NAME HERE]
+ *
+ * Written on March 5, 2023 for CS2 @ Menlo School
+ *
+ * DO NOT MODIFY MAIN OR ANY OF THE METHOD HEADERS.
+ */
+public class SpellingBee {
+
+    private String letters;
+    private ArrayList<String> words;
+    public static final int DICTIONARY_SIZE = 143091;
+    public static final String[] DICTIONARY = new String[DICTIONARY_SIZE];
+
+    public SpellingBee(String letters) {
+        this.letters = letters;
+        words = new ArrayList<String>();
+    }
+
+    // TODO: generate all possible substrings and permutations of the letters.
+    //  Store them all in the ArrayList words. Do this by calling ANOTHER method
+    //  that will find the substrings recursively.
+    public void generate() {
+        // YOUR CODE HERE — Call your recursive method!
+        makeWords("", words);
+    }
+
+    // Recursive method to find all possibly substrings
+    public void makeWords(String letters, ArrayList<String> words)
+    {
+        if (letters.isEmpty())
+        {
+            for (String word : words)
+            {
+                permute("", word);
+            }
+        }
+        words.add(letters);
+        for (int i = 0; i < letters.length(); i++)
+        {
+            String sub = letters.substring(0, i) + letters.substring(i+1);
+            System.out.println(sub);
+            makeWords(sub, words);
+        }
+    }
+
+    public void permute(String letts, String word) {
+        if (word.isEmpty()) {
+            words.add(letts);
+            return;
+        }
+        for (int i = 0; i < word.length(); i++) {
+            char newLetter = word.charAt(i);
+            String restWord = word.substring(0, i) + word.substring(i+1);
+            permute(letts + newLetter,  restWord);
+        }
+    }
+    // TODO: Apply mergesort to sort all words. Do this by calling ANOTHER method
+    //  that will find the substrings recursively.
+    public void sort() {
+        // YOUR CODE HERE
+        makeWords("", words);
+
+    }
+
+    // Removes duplicates from the sorted list.
+    public void removeDuplicates() {
+        int i = 0;
+        while (i < words.size() - 1) {
+            String word = words.get(i);
+            if (word.equals(words.get(i + 1)))
+                words.remove(i + 1);
+            else
+                i++;
+        }
+    }
+
+    // TODO: For each word in words, use binary search to see if it is in the dictionary.
+    //  If it is not in the dictionary, remove it from words.
+    public void checkWords() {
+        // YOUR CODE HERE
+        int low;
+        int high;
+        int mid;
+        for (int i = 0; i < words.size(); i++)
+        {
+            boolean isIn = false;
+            low = 0;
+            high = DICTIONARY_SIZE - 1;
+            while (low <= high)
+            {
+                mid = (high + low)/2;
+                if (DICTIONARY[mid].compareTo(words.get(i)) < 0)
+                {
+                    low = mid + 1;
+                }
+                else if (DICTIONARY[mid].compareTo(words.get(i)) > 0)
+                {
+                    high = mid - 1;
+                }
+                else if (DICTIONARY[mid].equals(words.get(i)))
+                {
+                    isIn = true;
+                    break;
+                }
+            }
+            if(!isIn) {
+                words.remove(words.get(i));
+            }
+        }
+    }
+
+    // Prints all valid words to wordList.txt
+    public void printWords() throws IOException {
+        File wordFile = new File("Resources/wordList.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(wordFile, false));
+        for (String word : words) {
+            writer.append(word);
+            writer.newLine();
+        }
+        writer.close();
+    }
+
+    public ArrayList<String> getWords() {
+        return words;
+    }
+
+    public void setWords(ArrayList<String> words) {
+        this.words = words;
+    }
+
+    public SpellingBee getBee() {
+        return this;
+    }
+
+    public static void loadDictionary() {
+        Scanner s;
+        File dictionaryFile = new File("Resources/dictionary.txt");
+        try {
+            s = new Scanner(dictionaryFile);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open dictionary file.");
+            return;
+        }
+        int i = 0;
+        while(s.hasNextLine()) {
+            DICTIONARY[i++] = s.nextLine();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Prompt for letters until given only letters.
+        Scanner s = new Scanner(System.in);
+        String letters;
+        do {
+            System.out.print("Enter your letters: ");
+            letters = s.nextLine();
+        }
+        while (!letters.matches("[a-zA-Z]+"));
+
+        // Load the dictionary
+        SpellingBee.loadDictionary();
+
+        // Generate and print all valid words from those letters.
+        SpellingBee sb = new SpellingBee(letters);
+        sb.generate();
+        sb.sort();
+        sb.removeDuplicates();
+        sb.checkWords();
+        try {
+            sb.printWords();
+        } catch (IOException e) {
+            System.out.println("Could not write to output file.");
+        }
+        s.close();
+    }
+}
